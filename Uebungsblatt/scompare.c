0870edf824a6f515f33472f187b60b9258af4203
Sipsel <daniel.franke19@gmail.com>
2020-06-17 19:23:50 +0200

    Update scompare.c

--- Uebungsblatt/scompare.c
+++ Uebungsblatt/scompare.c
@@ -4,20 +4,18 @@
 int scompare(char *txt1,char *txt2,int case_senstiv);
 int main()
 {
-    char txt1[] = "this is a dest string";
+    char txt1[] = "dhis is a test string";
     char txt2[] = "this is a test string";
-    printf("%d",scompare(txt1,txt2,0));
+    printf("%d\n",scompare(txt1,txt2,0));
 }
 int scompare(char *txt1,char *txt2,int case_senstiv)
 {
-    int x;
-    int to_upper_dif = ((int)'a')-((int)'A');
     if(strlen(txt1)!=strlen(txt2)){return 0;}
-    x = strcmp(txt2,txt1);
+    int x = strcmp(txt2,txt1); // either 1 for diffrent string, 0 for same string or 32/-32 for higher or lower letter
     if(case_senstiv)
     {
-        if(x==to_upper_dif){return 1;};
+        if((x==32) ||(x==-32)){return 1;}; //check if ascii is upper or lower
     }
     
-    return !x;
+    return (x == 0) ? 1 : 0;
 }
