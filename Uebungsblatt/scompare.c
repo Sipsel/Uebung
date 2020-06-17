3b4371678a0945ce7f12f8de2c2df0cc20875df4
Sipsel <daniel.franke19@gmail.com>
2020-06-17 19:11:45 +0200

    Update scompare.c

--- Uebungsblatt/scompare.c
+++ Uebungsblatt/scompare.c
@@ -6,17 +6,18 @@ int main()
 {
     char txt1[] = "this is a dest string";
     char txt2[] = "this is a test string";
-    printf("%d",scompare(txt1,txt2,1));
+    printf("%d",scompare(txt1,txt2,0));
 }
 int scompare(char *txt1,char *txt2,int case_senstiv)
 {
     int x;
-    int to_upper = ((int)'a')-((int)'A');
+    int to_upper_dif = ((int)'a')-((int)'A');
     if(strlen(txt1)!=strlen(txt2)){return 0;}
     x = strcmp(txt2,txt1);
     if(case_senstiv)
     {
-        if(x ==32){return 1;};
+        if(x==to_upper_dif){return 1;};
     }
+    
     return !x;
 }
